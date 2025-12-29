09160307004b35f7cc0418993da7ac9fe50a904a
Shami2810 <shami.ssk2810@gmail.com>
2025-12-29 11:25:20 +0530

    learn terraform code

--- main.tf
+++ main.tf
@@ -1,25 +1,24 @@
-resource "vault_mount" "component" {
-  path     = "roboshop-dev"
-  type     = "kv-v2"
-  options = {
-    version = "2"
-    type    = "kv-v2"
-  }
-  description = "RoboShop Dev"
-}
-
-
-# resource "vault_mount" "secret-mounts" {
-#   for_each = var.secret-mounts
-#   path     = each.key
+# resource "vault_mount" "component" {
+#   path     = "roboshop-dev"
 #   type     = "kv-v2"
 #   options = {
 #     version = "2"
 #     type    = "kv-v2"
 #   }
-#   description = each.value["description"]
+#   description = "RoboShop Dev"
 # }
-#
+
+resource "vault_mount" "secret-mounts" {
+  for_each = var.secret-mounts
+  path     = each.key
+  type     = "kv-v2"
+  options = {
+    version = "2"
+    type    = "kv-v2"
+  }
+  description = each.value["description"]
+}
+
 # resource "vault_kv_secret_v2" "secrets" {
 #   depends_on = [vault_mount.secret-mounts]
 #   for_each   = var.secrets
