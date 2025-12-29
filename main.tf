a00d4ffb3d004590f85bf3294d22ab7dae8c80d0
Shami2810 <shami.ssk2810@gmail.com>
2025-12-29 11:18:49 +0530

    learn terraform code

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "vault_mount" "component" {
-  path     = "cart-dev"
+  path     = "roboshop-dev"
   type     = "kv-v2"
   options = {
     version = "2"
     type    = "kv-v2"
   }
-  description = "Cart Dev"
+  description = "RoboShop Dev"
 }
 
 
